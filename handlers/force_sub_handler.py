7fe583957db3573d766e131645eea76a8bb3f739
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2024-06-06 14:15:48 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -32,7 +32,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/+4c-iaMUwPg44MGJl).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/Dotmoviesflix).",
                 disable_web_page_preview=True
             )
             return 400
